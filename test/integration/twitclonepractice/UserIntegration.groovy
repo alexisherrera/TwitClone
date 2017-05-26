235afa5868777e54ba1056ebde2adbe85a54b66f
Herrera <Alexis.Herrera@ellucian.com>
2017-05-26 10:39:01 -0400

    added tests

--- test/integration/twitclonepractice/UserIntegration.groovy
+++ test/integration/twitclonepractice/UserIntegration.groovy
@@ -24,8 +24,44 @@ class UserIntegration extends GrailsUnitTestCase {
     assertNotNull (user2.id)
 
     def foundUser1 = User.get(user1.id)
-    assertEquals ('Alexis', foundUser.user1.Id)
-    assertEquals('Lily', foundUser.user2.Id)
+    def foundUser2 = User.get(user2.id)
+    assertEquals ('Alexis', foundUser1.userId)
+    assertEquals('Lily', foundUser2.userId)
+
+    user2.userId = "Maria"
+    user2.save()
+    foundUser2 = User.get(user2.id)
+    assertEquals("Maria", foundUser2.userId)
+  }
+
+  //test our constraints our working
+  void testUserConstraints() {
+    def user1 = User([userId: 'Alexis', password: 'pass'])
+    assertFalse(user1.validate())
+    assertTrue(user1.hasErrors())
+  }
+
+  //test following
+  void testFollowing() {
+    def user1 = User([userId: 'Alexis', password: 'password'])
+    def user2 = User([userId: 'Lily', password: 'password'])
+    def user3 = User([userId: 'Yessy', password: 'password'])
+
+    user1.save()
+    user2.save()
+    user3.save()
+
+    user1.addToFollowing(user2)
+    user1.addToFollowing(user3)
+
+    assertEquals(2, user1.following.size())
+    def list = [user1.following[0].userId, user1.following[1].userId]
+    list.sort()
+    assertEquals(['Lily', 'Yessy'], list)
+
+    user2.addToFollowing(user1)
+    assertEquals(1, user2.following.size())
+    assertEquals(['Alexis'], [user2.following[0].userId])
   }
 
 }
